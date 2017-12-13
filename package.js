a74c4b62d23ac1cbf583e3813f8e7b09aa1130e4
stefanocudini <stefano.cudini@gmail.com>
2017-12-13 03:48:40 +0100

    update versions

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'keplerjs:robots',
   summary: 'keplerjs plugin to create various robot users to test interactions on the platform',
-  version: "1.2.9",
+  version: "1.3.0",
   git: "https://github.com/Keplerjs/Kepler.git"
 });
 
@@ -16,8 +16,7 @@ Package.onUse(function(api) {
   api.versionsFrom("1.5.1");
 
   api.use([
-    'keplerjs:core@1.2.3',
-    'keplerjs:admin@1.2.3'
+    'keplerjs:core@1.2.3'
   ]);
 
   api.addFiles([
